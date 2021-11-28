2c0daa53bfb751855e3bc8131ea6b4975a3e4de9
lhssluiz <lzhenriquesa@gmail.com>
2021-11-28 18:58:27 -0300

    All classes and projects from the last module of Angular

--- Módulo5_Angular/ProjetoFinalAngular_LuizSa/FRONT/kanban/src/app/app.module.ts
+++ Módulo5_Angular/ProjetoFinalAngular_LuizSa/FRONT/kanban/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardsTableComponent } from './cards-table/cards-table.component';
 import { ContainerComponent } from './container/container.component';
+import { CardsComponent } from './container/cards/cards.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsTableComponent,
-    ContainerComponent
+    ContainerComponent,
+    CardsComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Módulo5_Angular/ProjetoFinalAngular_LuizSa/FRONT/kanban/src/app/container/container.component.ts
+++ Módulo5_Angular/ProjetoFinalAngular_LuizSa/FRONT/kanban/src/app/container/container.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Card } from '../models/cards.model';
+import { ApiServices } from '../services/api.services';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
+
 export class ContainerComponent implements OnInit {
 
-  constructor() { }
+  cards!:Card;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiServices,
+    private router: Router
+
+  ) { }
 
   ngOnInit(): void {
+    this.getAllCardsFromAPI();
   }
 
+  getAllCardsFromAPI() {
+      this.api.getAllCards().subscribe((cards) => {
+        if (!cards) {
+          return;
+        } else {
+          this.cards = cards;
+        }
+      });
+
+}
 }
